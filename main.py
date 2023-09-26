23cb6f8efb0cfecaa4d0ad62ee11a861e237560f
sakatoko <roma28055@mail.ru>
2023-09-26 21:28:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+Autowithdraw with telegram tapping about success transcation, full source code.
+
+
+Автовывод с отстуком в телеграм о успешных транзакциях, полностью открытый код, исходники.
+
+
+t.me/drrfree - dev
+t.me/autowithdraw_crypto + t.me/autowithdraw_crypto_eng - groups
+
